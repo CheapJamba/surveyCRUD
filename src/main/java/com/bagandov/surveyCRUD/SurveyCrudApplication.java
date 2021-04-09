c8783b26a405971321c7c9ddd08e3bb060c49edc
Dzhanbolat <sattelizer952@gmail.com>
2021-04-09 18:12:10 +0700

    init

---
+++ src/main/java/com/bagandov/surveyCRUD/SurveyCrudApplication.java
@@ -0,0 +1,13 @@
+package com.bagndov.surveyCRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveyCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurveyCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bagandov/surveyCRUD/SurveyCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bagndov.surveyCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveyCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
