22aa39fd9a58a21ee648fc9cd43defcbb3375619
Dzhanbolat <sattelizer952@gmail.com>
2021-04-09 18:22:33 +0700

    controller

---
+++ src/main/java/com/bagandov/surveyCRUD/controller/SurveyController.java
@@ -0,0 +1,59 @@
+package com.bagandov.surveyCRUD.controller;
+
+import com.bagandov.surveyCRUD.db.infrastructure.SortFilterDTO;
+import com.bagandov.surveyCRUD.model.survey.Survey;
+import com.bagandov.surveyCRUD.service.SurveyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("survey")
+public class SurveyController {
+
+    private SurveyService surveyService;
+
+    @Autowired
+    public SurveyController(SurveyService surveyService) {
+        this.surveyService = surveyService;
+    }
+
+    @GetMapping
+    public List<Survey> getSurveys(@RequestBody SortFilterDTO sortFilterDTO) {
+        return surveyService.findAll(sortFilterDTO);
+    }
+
+    @GetMapping("/{surveyName}")
+    public Survey getSurveyByName(@PathVariable("surveyName") Long targetId) {
+
+        return surveyService.findOneById(targetId);
+
+//        try {
+//
+//        } catch (RuntimeException e) {
+//            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+//                    e.getMessage(), e);
+//        }
+
+    }
+
+    @PostMapping
+    public Survey addSurvey(@RequestBody Survey surveyToSave) {
+        surveyService.saveOne(surveyToSave);
+
+        return surveyToSave;
+    }
+
+    @PutMapping
+    public void updateExistingSurvey(@RequestBody Survey updatedSurvey) {
+        surveyService.saveOne(updatedSurvey);
+    }
+
+    @DeleteMapping("/{surveyName}")
+    public void deleteSurveyByName(@PathVariable("surveyName") Long targetId) {
+
+        surveyService.deleteOneById(targetId);
+    }
+
+}
